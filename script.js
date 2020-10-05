501e43342168ce7b670d3f603cd4ce0cef2289ac
Lance Ellis <scdelance@gmail.com>
2020-10-05 11:05:45 -0500

    fixing blog transition TODO: remove html from pages
    Author: Lance Ellis

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ barba.use(barbaCss)
 barba.init({
     transitions: [{
         name: "fade",
+        beforeEnter({current, next, trigger}){
+            const bodyLinks = document.querySelectorAll("body")
+            const href = next.url.path
+
+            if(href != "/blog.html"){
+                bodyLinks[0].classList.remove("blog")
+            }
+            else{
+                bodyLinks[0].classList.add("blog")
+            }
+        },
         afterEnter(){
             $("#header").load("template.html #default-header")
         }
