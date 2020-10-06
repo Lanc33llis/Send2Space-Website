949502901c7b5533027042fb672324de263693a2
Lance Ellis <scdelance@gmail.com>
2020-10-06 17:51:09 -0500

    ??

--- makecontact.js
+++ makecontact.js
@@ -1,12 +1,17 @@
-function submit(event) {
-    let name = form.elements["name"].value
-    let email = form.elements["email"].value
-    let message = form.elements["message"].value
 
-    console.log(name)
-    console.log(email)
-    console.log(message)
-  }
+
+function submit(event) {
+  let name = form.elements["name"].value
+  let email = form.elements["email"].value
+  let message = form.elements["message"].value
+
+  console.log(name)
+  console.log(email)
+  console.log(message)
+
+  
+  event.preventDefault()
+}
   
 const form = document.getElementById("contact");
 form.addEventListener("submit", submit);
